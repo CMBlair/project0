55083ab7f2f81f5b03770e3cbe7acdebb4fcd008
CMBlair <corey514@revature.net>
2022-06-21 11:44:56 -0400

    delete/create/import student
    added delete/create/import functionality, added CSV for import function

--- student.py
+++ student.py
@@ -1,5 +1,6 @@
 
 import pymysql
+import csv
 connection = pymysql.connect(host='localhost',
                              user='root',
                              password='',
@@ -9,14 +10,29 @@ cursor = connection.cursor()
 
 
 class Student():
-    def createStudent(self, name, major):
+    def createStudent(name, major):
         try:
             with connection.cursor() as cursor:
-                sql = 'INSERT INTO student (name, major) VALUES (%s, (SELECT ID FROM majors WHERE lower(Major_Name) = %s))'
+                sql = 'INSERT INTO student (name, major) VALUES (%s, %s)'
                 cursor.execute(sql, (name, major))
                 connection.commit()
                 print(f"student:>> {name} created")
         except pymysql.Error  as e:
             print(f"There was an error creating user: {e}")
-   
-Student.createStudent(Student, "Johnny", "veterinary science")
+    def deleteStudent(studentID):
+                try:
+                        with connection.cursor() as cursor:
+                                sql = 'DELETE FROM student WHERE lower(name) = %s'
+                                cursor.execute(sql, (studentID))
+                                connection.commit()
+                                print(f"student {studentID} has been deleted.")
+                except pymysql.Error as e:
+                        print(f"There was an error deleting the student {e}")
+    def importStudents(fileName):
+        csv_data = csv.reader(open(fileName))
+        for row in csv_data:
+            cursor.execute('INSERT INTO student(name,major) VALUES(%s, %s)', row)
+        connection.commit()
+        print("Students Imported into Database")
+Student.createStudent("Johnny", 12)
+Student.importStudents("studentsList.csv")
